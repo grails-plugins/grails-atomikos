17d5f75352fbc1a6eb361eb55911abed3cd13ebb
Burt Beckwith <burt@burtbeckwith.com>
2011-06-14 12:25:01 -0400

    initial commit

---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,28 @@
+grails.project.class.dir = 'target/classes'
+grails.project.test.class.dir = 'target/test-classes'
+grails.project.test.reports.dir = 'target/test-reports'
+grails.project.docs.output.dir = 'docs' // for backwards-compatibility, the docs are checked into gh-pages branch
+
+grails.project.dependency.resolution = {
+
+	inherits 'global'
+
+	log 'warn'
+
+	repositories {
+		grailsPlugins()
+		grailsHome()
+		grailsCentral()
+		mavenCentral()
+	}
+
+	dependencies {
+		compile('com.atomikos:atomikos-util:3.7.0')           { transitive = false }
+		compile('com.atomikos:transactions:3.7.0')            { transitive = false }
+		compile('com.atomikos:transactions-api:3.7.0')        { transitive = false }
+		compile('com.atomikos:transactions-hibernate3:3.7.0') { transitive = false }
+		compile('com.atomikos:transactions-jdbc:3.7.0')       { transitive = false }
+		compile('com.atomikos:transactions-jta:3.7.0')        { transitive = false }
+		compile('com.atomikos:transactions-jms:3.7.0')        { transitive = false }
+	}
+}
---
+++ src/groovy/grails/plugin/atomikos/UserTransactionServiceProperties.groovy
@@ -0,0 +1,37 @@
+/* Copyright 2011 SpringSource.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package grails.plugin.atomikos
+
+import org.codehaus.groovy.grails.commons.GrailsApplication
+import org.springframework.beans.factory.FactoryBean
+
+/**
+ * Allows properties to be set from the Grails config and doesn't require a properties file.
+ *
+ * @author <a href='mailto:burt@burtbeckwith.com'>Burt Beckwith</a>
+ */
+class UserTransactionServiceProperties implements FactoryBean<Properties> {
+
+	/** Dependency injection for the application. */
+	GrailsApplication application
+
+	Properties getObject() {
+		new Properties(application.config.grails.plugin.atomikos.uts)
+	}
+
+	Class<Properties> getObjectType() { Properties }
+
+	boolean isSingleton() { true }
+}
